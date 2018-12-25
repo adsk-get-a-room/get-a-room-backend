00316b3abbc265d4e0a184597d48f54404779c12
Adi Sharet <adi.sharet@autodesk.com>
2018-12-25 11:54:23 +0200

    Add API validations to post status, add name support

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var app = require('../app');
+const express = require('express');
+const router = express.Router();
+const app = require('../app');
+const { check, validationResult } = require('express-validator/check');
 
-router.post('/', function(req, res, next) {
-    var status = {id: req.body.id, is_occupied: req.body.is_occupied };
+
+router.post('/', [
+        check('id').isNumeric(),
+        check('name').isString().isLength({ max:200 }),
+        check('is_occupied').optional({nullable: true}).isBoolean()
+    ],
+    (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    let status = {
+        id: req.body.id,
+        name: req.body.name,
+        is_occupied: req.body.is_occupied
+    };
     app.status[status.id] = status;
-    res.send(status);
+    res.json(status);
 });
 
 router.get('/', function(req, res, next) {
