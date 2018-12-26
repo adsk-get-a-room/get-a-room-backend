eaca62195f17e6594116084183239b2c25e595c8
Adi Sharet <adi.sharet@autodesk.com>
2018-12-26 14:24:12 +0200

    Critical emoji fix

--- routes/slack.js
+++ routes/slack.js
@@ -8,7 +8,7 @@ router.post('/', [],
         let slackResult = '*Those rooms are free now:*\n';
         const allRoomStatus = await getAllRoomsStatus();
         allRoomStatus.filter(s => s.isOccupied === false)
-            .forEach(status => slackResult += '\n:jenkins_success: Room ' + status.id + ' (' + status.name + ')')
+            .forEach(status => slackResult += '\n:white_check_mark: Room ' + status.id + ' (' + status.name + ')')
 
         res.json({'text': slackResult});
     });
