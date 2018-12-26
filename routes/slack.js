44416907d506e23435a1e1110d40fd9f4bda28d0
Adi Sharet <adi.sharet@autodesk.com>
2018-12-26 14:20:02 +0200

    Refactor and make slake command work

--- routes/slack.js
+++ routes/slack.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
+const getAllRoomsStatus = require('../statusManager')
+
 
 router.post('/', [],
-    (req, res) => {
-        res.json('hello');
+    async (req, res) => {
+        let slackResult = '*Those rooms are free now:*\n';
+        const allRoomStatus = await getAllRoomsStatus();
+        allRoomStatus.filter(s => s.isOccupied === false)
+            .forEach(status => slackResult += '\n:jenkins_success: Room ' + status.id + ' (' + status.name + ')')
+
+        res.json({'text': slackResult});
     });
 
 module.exports = router;
